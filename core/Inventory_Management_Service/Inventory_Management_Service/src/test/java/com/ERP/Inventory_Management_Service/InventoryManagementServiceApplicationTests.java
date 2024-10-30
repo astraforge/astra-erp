5dfb3b8474a5f98aacc370ccbd63712a453712b9
Sarthak9111 <sarthakraghuvanshi1005@gmail.com>
2024-10-29 23:02:47 -0500

    Project Initialization

---
+++ core/Inventory_Management_Service/Inventory_Management_Service/src/test/java/com/ERP/Inventory_Management_Service/InventoryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ERP.Inventory_Management_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
